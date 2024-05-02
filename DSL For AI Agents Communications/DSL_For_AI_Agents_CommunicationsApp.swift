e15c8aa5e71e42b3ed5aba1b3bc1c06c2ba0a87b
VintusS <dragomir.mindrescu@isa.utm.md>
2024-05-02 11:54:35 +0300

    Initial Commit

---
+++ DSL For AI Agents Communications/DSL_For_AI_Agents_CommunicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSL_For_AI_Agents_CommunicationsApp.swift
+//  DSL For AI Agents Communications
+//
+//  Created by Dragomir Mindrescu on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DSL_For_AI_Agents_CommunicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
